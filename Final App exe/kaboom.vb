45d7f67cc857fcd741df317c6a98869483819174
DJWild2996 <d.j.wild2996@gmail.com>
2023-02-22 13:57:37 +1300

    Code

---
+++ Final App exe/kaboom.vb
@@ -0,0 +1,10 @@
+﻿
+Namespace My.Resources
+    Class kaboom
+
+        Shared Function wav() As String
+            Throw New NotImplementedException
+        End Function
+
+    End Class
+End Namespace
